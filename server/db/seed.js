d110c56c527b37248bfe8f5aeb0b38c4e0e5d159
Basam Hassan <basammoses@gmail.com>
2023-03-09 11:29:32 -0500

    made sure that user handle is unique

---
+++ server/db/seed.js
@@ -0,0 +1,57 @@
+import './connection.js'
+import 'dotenv/config'
+import User from '../models/users.js'
+import Post from '../models/posts.js'
+import mongoose from 'mongoose'
+
+await User.deleteMany({})
+await Post.deleteMany({})
+
+const myself = await User.create(
+  {
+    username: 'Basam',
+    handle: 'basammoses'
+  },
+  )
+  
+  await Post.insertMany([
+  {
+    
+    text: 'Hello World',
+    date: '11/26/2001',
+    author: myself.handle
+
+    },
+    {
+      text: 'Goodbye World',
+      date: '11/26/2101',
+      author: myself.handle
+    },
+
+
+    
+
+  ]
+  )
+
+
+const newUser = await User.create(
+  {
+    username: 'bathsalt',
+    handle: 'bathsalt10'
+  },
+)
+  
+
+await Post.create(
+  {
+    text: 'i love taking hot showers',
+    date: '10/06/2045',
+    author: newUser.handle
+  },  
+)
+
+console.log('Done seeding database')
+await mongoose.connection.close()
+  
+
--- server/models/posts.js
+++ server/models/posts.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+//import { userSchema } from "./users.js";
 
 
 const postSchema = new mongoose.Schema({
-  author: String,
+  author: {
+    ref: "User",
+    type: mongoose.Schema.Types.String
+  },
   text: String,
   date: Date,
 });
