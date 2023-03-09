711cfae0a897993f0d38a63b6c30ee3d76f297d8
Basam Hassan <basammoses@gmail.com>
2023-03-09 09:41:54 -0500

    start

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import Express from "express";
+
+const app = Express();
+
+
+app.get("/", (req, res) => {
+  return res.json ({ message: "Hello World" });
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+}
+)
